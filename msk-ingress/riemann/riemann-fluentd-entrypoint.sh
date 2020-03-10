5afdb259ff4ce391bdfb56d5fd89f95c95d01a24
Lee Myring <accounts@thinkstack.io>
2020-03-10 09:06:22 +0000

    TEL-1753 put all fluentd config into main folder
    * the docker compose will also build the riemann-fluentd image

--- msk-ingress/riemann/riemann-fluentd-entrypoint.sh
+++ msk-ingress/riemann/riemann-fluentd-entrypoint.sh
@@ -3,19 +3,19 @@
 #source vars if file exists
 DEFAULT=/etc/default/fluentd
 
-if [ -r $DEFAULT ]; then
+if [[ -r ${DEFAULT} ]]; then
     set -o allexport
-    . $DEFAULT
+    . ${DEFAULT}
     set +o allexport
 fi
 
 # If the user has supplied only arguments append them to `fluentd` command
-if [ "${1#-}" != "$1" ]; then
+if [[ "${1#-}" != "$1" ]]; then
     set -- fluentd "$@"
 fi
 
 # If user does not supply config file or plugins, use the default
-if [ "$1" = "fluentd" ]; then
+if [[ "$1" = "fluentd" ]]; then
     if ! echo $@ | grep ' \-c' ; then
        set -- "$@" -c /fluentd/etc/${FLUENTD_CONF}
     fi
